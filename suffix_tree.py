ade1b8e070725dd35c1438b0463f81a592fcafad
Jiangchen <zjcsjtu@umich.edu>
2019-04-15 00:59:14 -0400

    Implemented suffix tree init function

--- suffix_tree.py
+++ suffix_tree.py
@@ -42,6 +42,7 @@ class SuffixTree:
         for i in range(2, len(s) + 1):
             sufx = s[-i:]
             self.init_helper(cur_node=self.root, suffix=sufx, index=len(s) - i)
+            self.show_tree()
     
     def init_helper(self, cur_node, suffix, index):
         """Helper function for init method, recursively go down the tree."""
@@ -67,30 +68,32 @@ class SuffixTree:
         else:
             common_prefix = get_common_prefix(suffix, target_child.edge_label)
             common_pre_len = len(common_prefix)
-            # need to split the cur_node into two nodes
-            new_node = Node()
-            new_node.leaf_label = target_child.leaf_label
-            new_node.children = target_child.children.copy()
-            new_node.edge_label = target_child.edge_label[common_pre_len:]
-            target_child.leaf_label = -1
-            target_child.children.clear()
-            target_child.children.add(new_node)
-            target_child.edge_label = common_prefix
-            new_node.parent = target_child
+            if common_pre_len < len(target_child.edge_label):
+                # need to split the cur_node into two nodes
+                new_node = Node()
+                new_node.leaf_label = target_child.leaf_label
+                new_node.children = target_child.children.copy()
+                new_node.edge_label = target_child.edge_label[common_pre_len:]
+                target_child.leaf_label = -1
+                target_child.children.clear()
+                target_child.children.add(new_node)
+                target_child.edge_label = common_prefix
+                new_node.parent = target_child
             # create another node for the suffix
             suf_node = Node()
             suf_node.leaf_label = index
             suf_node.edge_label = suffix[common_pre_len:]
             suf_node.parent = target_child
+            target_child.children.add(suf_node)
+
 
     def show_tree(self):
         """Print the suffix tree level by level for debug."""
-        print('Printing the suffix tree...')
+        print('Printing the suffix tree level by level...')
         tmp_list = list(self.root.children)
-        print(tmp_list)
         ind = 1
         while tmp_list:
-            print(ind)
+            print('level', ind)
             next_children = []
             for child in tmp_list:
                 print(child.edge_label, '\t', child.leaf_label)
