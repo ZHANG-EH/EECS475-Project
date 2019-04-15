5adbd64551c8220d90b3d9ee2381ad550464a185
Jiangchen <zjcsjtu@umich.edu>
2019-04-15 19:31:40 -0400

    Updated sample run script

--- run.py
+++ run.py
@@ -2,9 +2,20 @@ import substr_enc as se
 
 
 def main():
+    """Run the paper example."""
     print('test 1')
     suffix_tree = se.utils.SuffixTree('cocoon')
     suffix_tree.show_tree()
+    r = suffix_tree.root
+    print(r.edge_label, r.get_path(), r.get_initial_path())
+    tmp_list = list(r.children)
+    while tmp_list:
+        next_children = []
+        for child in tmp_list:
+            print(child.edge_label, child.get_path(), child.get_initial_path())
+            next_children = next_children + list(child.children)
+        tmp_list = next_children
+
 
 if __name__ == '__main__':
     main()
