c125900b8ae024afb45289d1437e49d2421d61e7
Zhiqi Chen <zhiqich@umich.edu>
2019-04-16 16:27:28 -0400

    enc key_gen

--- substr_enc/substr_enc/enc/encrypt.py
+++ substr_enc/substr_enc/enc/encrypt.py
@@ -7,13 +7,17 @@ Enhao Zhang    <ehzhang@umich.edu>
 from substr_enc.utils import Node, SuffixTree
 from substr_enc.model import LAMBDA
 import random
+import secrets
 
 
 def key_gen():
     """Generate the keys according to paper."""
     # LAMBDA is 64
     print(LAMBDA)
-
+    key_list = []
+    for i in range(7):
+        key_list.append(secrets.token_bytes(LAMBDA))
+    return key_list
 
 
 def encrypt(k, s):
@@ -21,4 +25,11 @@ def encrypt(k, s):
     # build a tree upon the string s
     tree = SuffixTree(s)
     print('hi')
-    
+
+
+# def main():
+#     key_gen()
+#
+#
+# if __name__ == '__main__':
+#     main()
