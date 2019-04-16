2d8c9b64d6bbb7be7c0883ef9e531f4dda8e8a4a
Jiangchen <zjcsjtu@umich.edu>
2019-04-15 20:35:11 -0400

    Updated version to 0.1.1

--- substr_enc/setup.py
+++ substr_enc/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='substr_enc',
-    version='0.1.0',
+    version='0.1.1',
     packages=['substr_enc'],
     include_package_data=True,
     install_requires=[
