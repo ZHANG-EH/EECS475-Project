4dad561e9717e7e76bd299078bc6cdb4f20ee4c7
Jiangchen <zjcsjtu@umich.edu>
2019-04-19 22:52:32 -0400

    Updated server example

--- server.py
+++ server.py
@@ -11,20 +11,22 @@ def main():
     """A simple server answers clients questions."""
     # listen on port 6000
     sock = sc.sock_init(6000)
-    # block until it receives a question
-    question = sc.recv_data(sock)
-    print('Received:', question)
-    # send a feedback to port 6001
-    feedback = "Please give me some choices!"
-    print('Sending:', feedback)
-    sc.send_data(6001, feedback)
-    # receive choices
-    choices = sc.recv_data(sock)
-    print('Received:', choices)
-    # send the answer
-    answer = "My favorite is " + str(choices[0])
-    print('Sending:', answer)
-    sc.send_data(6001, answer)
+    while True:
+        print('I am a super AI and I am waiting for someone to ask me a question!')
+        # block until it receives a question
+        question = sc.recv_data(sock)
+        print('Received:', question)
+        # send a feedback to port 6001
+        feedback = "Please give me some choices!"
+        print('Sending:', feedback)
+        sc.send_data(6001, feedback)
+        # receive choices
+        choices = sc.recv_data(sock)
+        print('Received:', choices)
+        # send the answer
+        answer = "My favorite is " + str(choices[0])
+        print('Sending:', answer)
+        sc.send_data(6001, answer)
 
 
 if __name__ == '__main__':
