867f1c38b1c698fbd550408dfc5d1a3d05ef8984
Jiangchen <zjcsjtu@umich.edu>
2019-04-22 19:25:18 -0400

    Modified the query package and run script

--- substr_enc/setup.py
+++ substr_enc/setup.py
@@ -8,10 +8,11 @@ from setuptools import setup
 
 setup(
     name='substr_enc',
-    version='0.1.1',
+    version='0.1.2',
     packages=['substr_enc'],
     include_package_data=True,
     install_requires=[
         'pytest==4.3.0',
+        'pycrypto==2.6.1'
     ]
 )
