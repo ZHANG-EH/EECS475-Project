99c3514bf42307ed6b4d4e087f03deec37f2c887
Jiangchen <zjcsjtu@umich.edu>
2019-04-22 21:23:03 -0400

    Updated setup

--- substr_enc/setup.py
+++ substr_enc/setup.py
@@ -8,8 +8,8 @@ from setuptools import setup
 
 setup(
     name='substr_enc',
-    version='0.1.2',
-    packages=['substr_enc'],
+    version='0.1.4',
+    packages=['substr_enc', 'substr_enc.enc', 'substr_enc.utils', 'substr_enc.conn'],
     include_package_data=True,
     install_requires=[
         'pytest==4.3.0',
